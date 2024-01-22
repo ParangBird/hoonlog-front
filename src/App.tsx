87f7caf6d23120ae29084dcfdff504140dea5bc7
ParangBird <seekerman@naver.com>
2024-01-22 22:28:33 +0900

    feat: css와 html 사이에서 헤매는 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,7 +7,7 @@ function App() {
   return (    
     <div className="App">      
       <div>
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <nav className="m-0 p-0 bg-red-400 border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
                 {/* <img src="" className="h-8" alt="Hoonlog Logo" /> */}
@@ -22,16 +22,16 @@ function App() {
             <div className="hidden w-full md:block md:w-auto" id="navbar-default">
               <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                  <a href="/home" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                  <Link to="/home" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
                 </li>
                 <li>
-                  <a href="/blog" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
+                  <Link to="/blog" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</Link>
                 </li>
                 <li>
-                  <a href="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+                  <Link to="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
                 </li>
                 <li>
-                  <a href="/thoughts" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Thoughts</a>
+                  <Link to="/thoughts" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Thoughts</Link>
                 </li>
               </ul>
             </div>
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,6 +4,20 @@ type Props = {}
 
 export default function About({}: Props) {
   return (
-    <div>About me</div>
+    <>
+      <hr className="mx-auto m-1 h-10"/>
+      <div>Hello, I'm web developer</div>      
+      <div className="flex justify-end h-full bg-pink-400">
+        <div className="inline h-8 w-1/3 bg-red-400">
+          <h2 className="text-2xl text-blue-500">About me</h2>
+        </div>
+        <div className="w-1/3 bg-teal-400">
+          Something picture
+        </div>
+        <div className="w-1/3 bg-blue-400">
+          Details
+        </div>
+      </div>
+    </>        
   )
 }
