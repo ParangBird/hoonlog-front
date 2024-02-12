d693546cb6d26f6beeb18d41ed031a8fc85f91d8
ParangBird <seekerman@naver.com>
2024-02-12 21:24:28 +0900

    feat: Admin page 생성

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 type Props = {}
 
+const doLogin = () => {
+    console.log("login!!");
+}
 export default function About({}: Props) {
   return (
-    <>      
-        Are you Admin?
+    <>
+        Are you Admin? <p></p> <br/><br/><br/><br/><br/>
+        <TextField
+                  required
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  onClick={doLogin}
+        />
+        <p></p><br/>
+        <Button variant="contained" endIcon={<SendIcon />}>
+            LOGIN
+        </Button>           
     </>        
   )
 }
